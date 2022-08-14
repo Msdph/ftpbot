f603a0c3d58dbc49b3ee29f2a87f752651c00643
Msdph <99562192+Msdph@users.noreply.github.com>
2022-08-14 15:56:50 +0430

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 
-DB_NAME = os.environ.get("DB_NAME","pyro-botz")
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","masoudbiatomoviez")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://masoudbiatomoviez:12345678VB@cluster0.jigze.mongodb.net/Cluster0?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
